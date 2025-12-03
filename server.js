9afc01225859e93054dfd4600df4ae6372c8e9f1
Aryasatya2356 <lenathea0605@gmail.com>
2025-12-03 18:28:39 +0700

    menambahkan enpoint use test untuk deploy di vercel

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const db = require('./db.js');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Server Banyuwangi Marketplace (CRUD) Aktif!');
+});
+
+app.use((err,req,res,next) =>{ 
+    console.error('[SERVERERROR]',err.stack); 
+    res.status(500).json({error:'Terjadikesalahanpadaserver'}); 
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server aktif di http://localhost:${PORT}`);
+});

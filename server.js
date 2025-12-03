e05ca5075cfd5a25b5ef3bb425dd5fae284c691c
Aryasatya2356 <lenathea0605@gmail.com>
2025-12-03 18:40:12 +0700

    menambahkan endpoint /vendor-a

--- server.js
+++ server.js
@@ -12,6 +12,21 @@ app.get('/', (req, res) => {
     res.send('Server Banyuwangi Marketplace (CRUD) Aktif!');
 });
 
+app.get('/vendor-a', async (req, res, next) => {
+    try {
+        const result = await db.query('SELECT * FROM vendor_a ORDER BY id ASC');
+        // Mapping Database -> JSON Format Soal
+        const data = result.rows.map(row => ({
+            id_db: row.id, // ID Database (perlu untuk edit/delete)
+            kd_produk: row.kd_produk,
+            nm_brg: row.nm_brg,
+            hrg: row.hrg,
+            ket_stok: row.ket_stok
+        }));
+        res.json(data);
+    } catch (err) { next(err); }
+});
+
 app.use((err,req,res,next) =>{ 
     console.error('[SERVERERROR]',err.stack); 
     res.status(500).json({error:'Terjadikesalahanpadaserver'}); 

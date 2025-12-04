ffe6ac310ab15f4690e3f78e73c6a54c8eb9d0d0
Aryasatya2356 <lenathea0605@gmail.com>
2025-12-04 10:54:28 +0700

    menambahkan endpoint get vendor A

--- server.js
+++ server.js
@@ -12,20 +12,40 @@ app.get('/status', (req, res) => {
     res.json({ ok: true, service: 'film-api_punya arya' });
 });
 
-app.get('/vendor-a', async (req, res, next) => {
+// app.get('/vendor-a', async (req, res, next) => {
+//     try {
+//         const result = await db.query('SELECT * FROM vendor_a ORDER BY id ASC');
+//         // Mapping Database -> JSON Format Soal
+//         const data = result.rows.map(row => ({
+//             id_db: row.id, // ID Database (perlu untuk edit/delete)
+//             kd_produk: row.kd_produk,
+//             nm_brg: row.nm_brg,
+//             hrg: row.hrg,
+//             ket_stok: row.ket_stok
+//         }));
+//         res.json(data);
+//     } catch (err) { next(err); }
+// });
+
+app.get('/vendor-a', async(req, res, next) => {
+    const sql = 'SELECT id, kd_produk, nm_brg, hrg, ket_stok FROM vendor_a ORDER BY id ASC';
     try {
-        const result = await db.query('SELECT * FROM vendor_a ORDER BY id ASC');
-        // Mapping Database -> JSON Format Soal
-        const data = result.rows.map(row => ({
-            id_db: row.id, // ID Database (perlu untuk edit/delete)
-            kd_produk: row.kd_produk,
-            nm_brg: row.nm_brg,
-            hrg: row.hrg,
-            ket_stok: row.ket_stok
-        }));
-        res.json(data);
-    } catch (err) { next(err); }
+        const result = await db.query(sql);
+        res.json(result.rows);
+    } catch (err) {
+        next(err);
+    }
 });
+
+// app.get('/movies', async (req, res, next) => {
+//     const sql = 'SELECT m.id, m.title, m.year, d.id as director_id, d.name as director_name FROM movies m LEFT JOIN directors d ON m.director_id = d.id ORDER BY m.id ASC';
+//     try {
+//         const result = await db.query(sql);
+//         res.json(result.rows);
+//     } catch (err) {
+//         next(err);
+//     }
+// });
 
 app.use((req, res) => {
     res.status(404).json({ error: 'Rute tidak ditemukan' });

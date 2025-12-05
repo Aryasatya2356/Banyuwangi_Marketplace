43e19c0240e988288cc3532dfad2a6cb103cfd70
Aryasatya2356 <lenathea0605@gmail.com>
2025-12-05 10:49:30 +0700

    Normalisasi dan integrasi - by Arya

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ app.post('/auth/login', async (req, res, next) => {
 
 // vendor A Wempi kelompok 1
 app.get('/vendor-a', async(req, res, next) => {
-    const sql = 'SELECT id, kd_produk, nm_brg, hrg, ket_stok FROM vendor_a ORDER BY id ASC';
+    const sql = 'SELECT  kd_produk, nm_brg, hrg, ket_stok FROM vendor_a ORDER BY kd_product ASC';
     try {
         const result = await db.query(sql);
         res.json(result.rows);
@@ -88,10 +88,10 @@ app.get('/vendor-a', async(req, res, next) => {
     }
 });
 
-app.get('/vendor-a/:id', async (req, res, next)=>{
-    const sql = 'SELECT id, kd_produk, nm_brg, hrg, ket_stok FROM vendor_a where id = $1';
+app.get('/vendor-a/:kd_product', async (req, res, next)=>{
+    const sql = 'SELECT kd_produk, nm_brg, hrg, ket_stok FROM vendor_a where kd_product = $1';
     try {
-        const result = await db.query(sql, [req.params.id]);
+        const result = await db.query(sql, [req.params.kd_product]);
         if (result.rows.length ===0){
             return res.status(404).json({ error: 'Produk tidak ditemukan'});
         }
@@ -115,11 +115,11 @@ app.post('/vendor-a', authenticateToken, async (req, res, next) => {
     }
 });
 
-app.put('/vendor-a/:id', [authenticateToken, authorizeRole('admin')], async (req, res, next) => {
-    const {kd_produk, nm_brg, hrg, ket_stok} = req.body;
-    const sql ='UPDATE vendor_a SET kd_produk = $1, nm_brg =$2, hrg =$3, ket_stok =$4 WHERE id =$5 RETURNING *';
+app.put('/vendor-a/:kd_product', [authenticateToken, authorizeRole('admin')], async (req, res, next) => {
+    const {nm_brg, hrg, ket_stok} = req.body;
+    const sql ='UPDATE vendor_a SET nm_brg =$1, hrg =$2, ket_stok =$3 WHERE kd_product =$4 RETURNING *';
     try{
-        const result = await db.query(sql, [kd_produk, nm_brg, hrg, ket_stok, req.params.id]);
+        const result = await db.query(sql, [kd_produk, nm_brg, hrg, ket_stok, req.params.kd_product]);
         if  (result.rowCount === 0) {
             return res.status(404).json({error: 'Produk tidak ditemukan'});
         }
@@ -130,10 +130,10 @@ app.put('/vendor-a/:id', [authenticateToken, authorizeRole('admin')], async (req
 });
 
 
-app.delete('/vendor-a/:id', [authenticateToken, authorizeRole('admin')], async (req, res, next) =>{
-    const sql = 'DELETE FROM vendor_a WHERE id = $1 RETURNING *';
+app.delete('/vendor-a/:kd_product', [authenticateToken, authorizeRole('admin')], async (req, res, next) =>{
+    const sql = 'DELETE FROM vendor_a WHERE kd_product = $1 RETURNING *';
     try{
-        const result = await db.query(sql, [req.params.id]);
+        const result = await db.query(sql, [req.params.kd_product]);
         if (result.rowCount === 0 ) {
             return res.status(404).json({ error: 'Produk tidak ditemukan'});
         }
@@ -145,7 +145,7 @@ app.delete('/vendor-a/:id', [authenticateToken, authorizeRole('admin')], async (
 
 // vendor B Zul kelompok 1
 app.get('/vendor-b', async(req, res, next) => {
-    const sql = 'SELECT id, sku, "productName", price, "isAvailable" FROM vendor_b ORDER BY id ASC';
+    const sql = 'SELECT sku, "productName", price, "isAvailable" FROM vendor_b ORDER BY sku ASC';
     try {
         const result = await db.query(sql);
         res.json(result.rows);
@@ -154,10 +154,10 @@ app.get('/vendor-b', async(req, res, next) => {
     }
 });
 
-app.get('/vendor-b/:id', async (req, res, next)=>{
-    const sql = 'SELECT id, sku, "productName", price, "isAvailable" FROM vendor_b where id = $1';
+app.get('/vendor-b/:sku', async (req, res, next)=>{
+    const sql = 'SELECT sku, "productName", price, "isAvailable" FROM vendor_b where sku = $1';
     try {
-        const result = await db.query(sql, [req.params.id]);
+        const result = await db.query(sql, [req.params.sku]);
         if (result.rowCount.length === 0){
             return res.status(404).json({ error: 'Produk tidak ditemukan'});
         }
@@ -169,23 +169,23 @@ app.get('/vendor-b/:id', async (req, res, next)=>{
 
 app.post('/vendor-b', authenticateToken, async (req, res, next) => {
     const { sku, productName, price, isAvailable = undefined } = req.body;
-    if (!sku || !productName || !price || !isAvailable) {
-        return res.status(400).json({ error: 'sku, "productName", price, "isAvailable" wajib diisi'});
+    if (!sku ||!productName || !price || !isAvailable) {
+        return res.status(400).json({ error: '"productName", price, "isAvailable" wajib diisi'});
     }
-    const sql = 'INSERT INTO vendor_b (sku, "productName", price, "isAvailable") VALUES ($1, $2, $3, $4) RETURNING *';
+    const sql = 'INSERT INTO vendor_b (sku, "productName", price, "isAvailable") VALUES ($1, $2, $3 ) RETURNING *';
     try{
-        const result = await db.query(sql, [sku, productName, price, isAvailable]);
+        const result = await db.query(sql, [productName, price, isAvailable]);
         res.status(201).json(result.rows[0]);
     }catch (err){
         next(err);
     }
 });
 
-app.put('/vendor-b/:id', [authenticateToken, authorizeRole('admin')], async (req, res, next) => {
-    const {sku, productName, price, isAvailable } = req.body;
-    const sql ='UPDATE vendor_b SET sku = $1, "productName" =$2, price =$3, "isAvailable" =$4 WHERE id =$5 RETURNING *';
+app.put('/vendor-b/:sku', [authenticateToken, authorizeRole('admin')], async (req, res, next) => {
+    const { productName, price, isAvailable } = req.body;
+    const sql ='UPDATE vendor_b SET "productName" =$1, price =$2, "isAvailable" =$3 WHERE sku =$4 RETURNING *';
     try{
-        const result = await db.query(sql, [sku, productName, price, isAvailable, req.params.id]);
+        const result = await db.query(sql, [productName, price, isAvailable, req.params.sku]);
         if  (result.rowCount === 0) {
             return res.status(404).json({error: 'Produk tidak ditemukan'});
         }
@@ -196,10 +196,10 @@ app.put('/vendor-b/:id', [authenticateToken, authorizeRole('admin')], async (req
 });
 
 
-app.delete('/vendor-b/:id',[authenticateToken, authorizeRole('admin')], async (req, res, next) =>{
-    const sql = 'DELETE FROM vendor_b WHERE id = $1 RETURNING *';
+app.delete('/vendor-b/:sku',[authenticateToken, authorizeRole('admin')], async (req, res, next) =>{
+    const sql = 'DELETE FROM vendor_b WHERE sku = $1 RETURNING *';
     try{
-        const result = await db.query(sql, [req.params.id]);
+        const result = await db.query(sql, [req.params.sku]);
         if (result.rowCount === 0 ) {
             return res.status(404).json({ error: 'Produk tidak ditemukan'});
         }
@@ -216,8 +216,7 @@ app.get(`/vendor-c`, async (req, res, next) => {
     try {
         const result = await db.query(sql);
         const data = result.rows.map(row => ({
-            id: row.id,
-            id:row.custom_id,
+            id:row.id,
             details:{
                 name:row.name,
                 category:row.category,
@@ -246,7 +245,6 @@ app.get(`/vendor-c/:id`, async(req, res, next) => {
 
         const data = {
             id:row.id,
-            id:row.custom_id,
             details:{
                 name:row.name,
                 category:row.category,
@@ -270,7 +268,7 @@ app.post(`/vendor-c`, async(req, res, next) =>{
         return res.status(400).json({error: `Data JSON tidak lengkap(pastikan lengkap)`});
     }
 
-    const sql = 'INSERT INTO vendor_c (custom_id, name, category, base_price, tax, stock) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *';
+    const sql = 'INSERT INTO vendor_c (id, name, category, base_price, tax, stock) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *';
 
     try{
           const result = await db.query(sql, [
@@ -288,13 +286,12 @@ app.post(`/vendor-c`, async(req, res, next) =>{
 });
 
 app.put(`/vendor-c/:id`, async(req, res, next) => {
-    const{id, details, pricing, stock}=req.body;
+    const{details, pricing, stock}=req.body;
 
-    const sql = 'UPDATE vendor_c SET custom_id=$1, name=$2, category=$3, base_price=$4, tax=$5, stock=$6 WHERE ID =$7 RETURNING *';
+    const sql = 'UPDATE vendor_c SET name=$1, category=$2, base_price=$3, tax=$4, stock=$5 WHERE ID =$6 RETURNING *';
 
     try {
         const result = await db.query(sql, [
-            id,
             details.name,
             details.category,
             pricing.base_price,
@@ -337,7 +334,7 @@ app.get('/api/banyuwangi-marketplace', async (req, res, next) => {
         const [resA, resB, resC] = await Promise.all([
             fetch(`${baseUrl}/vendor-a`),
             fetch(`${baseUrl}/vendor-b`),
-            fetch(`${baseUrl}/status`),
+            fetch(`${baseUrl}/vendor-c`),
         ]);
 
         const dataA = await resA.json();
@@ -366,6 +363,20 @@ app.get('/api/banyuwangi-marketplace', async (req, res, next) => {
                 harga: item.price,
                 status:status,
                 sumber: "Vendor B"
+            };
+        });
+
+            const mapC = dataC.map(item => {
+            let finalPrice = item.pricing.base_price + item.pricing.tax;
+            let name = item.details.name;
+            if (item.details.category === 'Food') name += " (Recommended)";
+            let status = item.stock > 0 ? "Tersedia" : "Habis";
+            return {
+                id: item.id,
+                nama_produk: name,
+                harga: finalPrice,
+                status:status,
+                sumber: "Vendor C"
             };
         });
 
